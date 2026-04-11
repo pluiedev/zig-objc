fc7ceb1bc49882202415cd598d3996ae5a637ebe
Leah Amelia Chen <hi@pluie.me>
2026-04-11 12:28:07 +0800

    chore: update flake, use Zig 0.16.0-dev.3142+5ccfeb926

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Objective-C runtime bindings for Zig";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
     flake-utils.url = "github:numtide/flake-utils";
     zig.url = "github:mitchellh/zig-overlay";
 
     # Used for shell.nix
     flake-compat = {
-      url = github:edolstra/flake-compat;
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
@@ -35,7 +35,7 @@
       in rec {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            zigpkgs."0.15.1"
+            zigpkgs.master
           ];
         };
 
